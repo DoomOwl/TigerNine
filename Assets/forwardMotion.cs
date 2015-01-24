8d6aee775c6d4c6ea18df06f3f98307bbde67346
Alex Coulombe <alex.coulombe@gmail.com>
2015-01-23 23:36:07 -0500

    initial unity project commit

---
+++ Assets/forwardMotion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class forwardMotion : MonoBehaviour {
+
+	public int speed = 1;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * speed * Time.deltaTime;
+		//transform.localPosition += transform.forward * speed * Time.deltaTime;
+	}
+}
