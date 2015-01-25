be967d4b17d355f78e144597d8d887bf3e74e800
Max Dumas <maltor124@gmail.com>
2015-01-24 23:34:16 -0500

    Increased fleet warp delay.

--- Assets/Scripts/fleetWarpDelay.cs
+++ Assets/Scripts/fleetWarpDelay.cs
@@ -1,34 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class fleetWarpDelay : MonoBehaviour {
+public class fleetWarpDelay : MonoBehaviour
+{
 
-	private int launchDelay = 360;
-	private int launchRand = 60;
-	private bool notLaunched = true;
-	private float lifetime = 5f;
+		private int launchDelay = 400;
+		private int launchRand = 60;
+		private bool notLaunched = true;
+		private float lifetime = 5f;
 
-	// Use this for initialization
-	void Start () {
-		notLaunched = true;
-		//Destroy (gameObject, lifetime);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		int frameTime = Time.frameCount / 6;
-		//Debug.Log (frameTime);
-		if (frameTime > launchDelay && notLaunched == true) {
-			int launch = Random.Range(1,launchRand);
-			if (launch == 1) {
-				animation.Play("warpNew");
-				notLaunched = false;
-				Destroy (gameObject, lifetime);
-				return;
-				//Debug.Log (launch);
-			}
+		// Use this for initialization
+		void Start ()
+		{
+				notLaunched = true;
+				//Destroy (gameObject, lifetime);
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				int frameTime = Time.frameCount / 6;
+				//Debug.Log (frameTime);
+				if (frameTime > launchDelay && notLaunched == true) {
+						int launch = Random.Range (1, launchRand);
+						if (launch == 1) {
+								animation.Play ("warpNew");
+								notLaunched = false;
+								Destroy (gameObject, lifetime);
+								return;
+								//Debug.Log (launch);
+						}
+				}
+		}
 
 
 }
