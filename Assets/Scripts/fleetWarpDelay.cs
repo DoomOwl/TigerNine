e0ecdfb677de7db34c8f58588c7c131cef932a16
Alex Coulombe <alex.coulombe@gmail.com>
2015-01-24 21:16:28 -0500

    Alex updates master
    moving everything, stars are there, animation is there.

--- Assets/Scripts/fleetWarpDelay.cs
+++ Assets/Scripts/fleetWarpDelay.cs
@@ -3,21 +3,32 @@ using System.Collections;
 
 public class fleetWarpDelay : MonoBehaviour {
 
+	private int launchDelay = 80;
+	private int launchRand = 60;
+	private bool notLaunched = true;
+	private float lifetime = 5f;
+
 	// Use this for initialization
 	void Start () {
-	
+		notLaunched = true;
+		//Destroy (gameObject, lifetime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		int frameTime = Time.frameCount / 6;
 		//Debug.Log (frameTime);
-		if (frameTime > 10) {
-			int launch = Random.Range(1,15);
+		if (frameTime > launchDelay && notLaunched == true) {
+			int launch = Random.Range(1,launchRand);
 			if (launch == 1) {
 				animation.Play("warpNew");
+				notLaunched = false;
+				Destroy (gameObject, lifetime);
+				return;
 				//Debug.Log (launch);
 			}
 		}
 	}
+
+
 }
