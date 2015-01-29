a89416f416bd9969db0c0a4a9431a75587379cb2
Alex Coulombe <alex.coulombe@gmail.com>
2015-01-29 11:56:54 -0500

    fixed radio, added subtitle at beginning & credits

--- Assets/radioScript.cs
+++ Assets/radioScript.cs
@@ -20,7 +20,7 @@ public class radioScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		radioStation = "off";
+		radioStation = "gameRadio";
 	}
 	
 	// Update is called once per frame
@@ -42,31 +42,38 @@ public class radioScript : MonoBehaviour {
 				radioStation = "off";
 			}
 			
+			if(radioStation == "off"){
+				Radio1.audio.volume = 0;
+				Radio2.audio.volume = 0;
+				Radio3.audio.volume = 0;
+				Radio4.audio.volume = 0;
+				Debug.Log ("off");
+		}
 			if(radioStation == "gameRadio"){
 				Radio1.audio.volume = 1;
-				Radio2.audio.mute = true;
-				Radio3.audio.mute = true;
-				Radio4.audio.mute = true;
+				Radio2.audio.volume = 0;
+				Radio3.audio.volume = 0;
+				Radio4.audio.volume = 0;
 				Debug.Log ("Station01");
 			}
 			else if(radioStation == "spaceNPR"){
-				Radio1.audio.mute = true;
+				Radio1.audio.volume = 0;
 				Radio2.audio.volume = 1;
-				Radio3.audio.mute = true;
-				Radio4.audio.mute = true;
+				Radio3.audio.volume = 0;
+				Radio4.audio.volume = 0;
 				Debug.Log ("Station02");
 			}
 			else if(radioStation == "AlexMusic"){
-				Radio1.audio.mute = true;
-				Radio2.audio.mute = true;
+				Radio1.audio.volume = 0;
+				Radio2.audio.volume = 0;
 				Radio3.audio.volume = 1;
-				Radio4.audio.mute = true;
+				Radio4.audio.volume = 0;
 				Debug.Log ("Station03");
 		}
 			else if(radioStation == "MatMusic"){
-				Radio1.audio.mute = true;
-				Radio2.audio.mute = true;
-				Radio3.audio.mute = true;
+				Radio1.audio.volume = 0;
+				Radio2.audio.volume = 0;
+				Radio3.audio.volume = 0;
 				Radio4.audio.volume = 1;
 				Debug.Log ("Station04");
 			}
